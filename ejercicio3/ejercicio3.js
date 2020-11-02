773e0e8f1d005b83d4683c96f6768bea503dc742
Elver Tunubala <elvertunubala@gmail.com>
2020-11-01 21:50:12 -0500

    ejercicio3

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,2 +1,17 @@
-ejercicio3
+let ju;
+
+let suma=0;
+
+ju = Number(prompt('ingresar la edad de Juan'));
+
+al= parseInt((ju*2) / 3);
+an= parseInt((ju*4) / 3);
+
+suma= al+an+ju;
+
+document.write("La edad de la mama es  : " +suma + "<br>"+
+"La edad de Juan es  : " +ju + "<br>"+
+"La edad de Ana es  : " +an+"<br>"+
+"La edad de Alberto es  : " +al );
+
 
