936a6ba4e400a43698db4e161c30c491a2ab0802
Elver Tunubala <elvertunubala@gmail.com>
2020-11-01 19:54:50 -0500

    ejercicio1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -3,8 +3,10 @@ let p2;
 let p3;
 let ef;
 let tf;
-let par=55 / 3;
+
+
 let suma=0;
+par=55 / 3;
 
 p1 = Number(prompt('ingresar nota parcial 1'));
 p2 = Number(prompt('ingresar nota parcial 2'));
@@ -13,14 +15,14 @@ ef = Number(prompt('ingresar nota examen final '));
 tf = Number(prompt('ingresar nota trabajo final'));
 
 
-let p1= (par*p1)/100
-let p2= (par*p2)/100
-let p3= (par*p3)/100
+ p1= (par*p1)/100
+ p2= (par*p2)/100
+ p3= (par*p3)/100
 
-let ef= (30*ef)/100
-let tf= (15*tf)/100
+ ef= (30*ef)/100
+ tf= (15*tf)/100
 
-suma = p1+p2+p3+ef+tf)/5;
+suma = p1+p2+p3+ef+tf;
 
-console.log("la calificacion final del alumno es " +suma);
+document.write("La calificacion final de alumno es: " +suma);
 
