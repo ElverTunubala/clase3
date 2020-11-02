71cc82f21522617abbffd42c2d5012149e456e83
Elver Tunubala <elvertunubala@gmail.com>
2020-11-01 21:13:24 -0500

    ejercicio2

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -1,1 +1,21 @@
-ejercicio2
+let p1;
+let p2;
+let p3;
+
+let suma=0;
+
+p1 = Number(prompt('ingresar inversion 1'));
+p2 = Number(prompt('ingresar inversion 2'));
+p3 = Number(prompt('ingresar inversion 3'));
+
+suma= p1+p2+p3;
+
+p1= (p1/suma)*100;
+p2= (p2/suma)*100;
+p3= (p3/suma)*100;
+
+document.write("El porcentaje de la persona 1 es : " +p1+"%" + "<br>"+
+"El porcentaje de la persona 2 es : " +p2+"%" + "<br>"+
+"El porcentaje de la persona 3 es  : " +p3 + "%");
+
+
